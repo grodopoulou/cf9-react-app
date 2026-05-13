dde1a9966785f082913a2e5ea406c857952c9bfe
GeorgiaR <grodopoulou95@gmail.com>
2026-05-13 19:56:42 +0300

    feat: add Header, Footer and Layout components

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,31 @@
 
 
 import ArrowFunctionalComponentWithProps from "./components/LessonThree/ArrowFunctionalComponentWithProps.tsx";
+import Layout from "./components/Layout.tsx";
 
 function App() {
 
   return (
     <>
+        {/*Lesson 1*/}
       {/*<FunctionalComponent />*/}
       {/*<ClassComponent />*/}
       {/*<ArrowFunctionalComponent/>*/}
 
-        <ArrowFunctionalComponentWithProps title="Heading 1"/>
-        <ArrowFunctionalComponentWithProps
-            title="Second Title"
-            description="lorem20"
-        />
+        {/*Lesson 2*/}
+        {/*<ArrowFunctionalComponentWithProps title="Heading 1"/>*/}
+        {/*<ArrowFunctionalComponentWithProps*/}
+        {/*    title="Second Title"*/}
+        {/*    description="lorem20"*/}
+        {/*/>*/}
+
+        <Layout >
+            <ArrowFunctionalComponentWithProps
+                title="Second Title"
+                description ="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem."
+            />
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+        </Layout>
     </>
   )
 }
