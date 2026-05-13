4e4233787bf4dcbaae9762e67b1d01155b0a4287
GeorgiaR <grodopoulou95@gmail.com>
2026-05-13 19:23:37 +0300

    feat: add ArrowFunctionalComponentWithProps component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import FunctionalComponent from "./components/LessonTwo/FunctionalComponent.tsx";
-import ClassComponent from "./components/LessonTwo/ClassComponent.tsx";
-import ArrowFunctionalComponent from "./components/LessonTwo/ArrowFunctionalComponent.tsx";
+// import FunctionalComponent from "./components/LessonTwo/FunctionalComponent.tsx";
+// import ClassComponent from "./components/LessonTwo/ClassComponent.tsx";
+// import ArrowFunctionalComponent from "./components/LessonTwo/ArrowFunctionalComponent.tsx";
+
+
+import ArrowFunctionalComponentWithProps from "./components/LessonThree/ArrowFunctionalComponentWithProps.tsx";
 
 function App() {
 
   return (
     <>
-      <FunctionalComponent />
-      <ClassComponent />
-      <ArrowFunctionalComponent/>
+      {/*<FunctionalComponent />*/}
+      {/*<ClassComponent />*/}
+      {/*<ArrowFunctionalComponent/>*/}
 
+        <ArrowFunctionalComponentWithProps title="Heading 1"/>
+        <ArrowFunctionalComponentWithProps
+            title="Second Title"
+            description="lorem20"
+        />
     </>
   )
 }
