1b0799b47e46b9f5320d90487d76c9fa861f0951
GeorgiaR <grodopoulou95@gmail.com>
2026-05-20 15:03:53 +0300

    feat: add WindowSize component

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@
 // import ArrowFunctionalComponentWithProps from "./components/LessonThree/ArrowFunctionalComponentWithProps.tsx";
 import Layout from "./components/Layout.tsx";
 // import AutoRedirect from "./components/LessonSix/AutoRedirect.tsx";
-import AutoRedirectAdvanced from "./components/LessonSix/AutoRedirectAdvanced.tsx";
+// import AutoRedirectAdvanced from "./components/LessonSix/AutoRedirectAdvanced.tsx";
+import WindowSize from "./components/LessonSix/WindowSize.tsx";
 // import CounterAdvanced from "./components/LessonFour/CounterAdvanced.tsx";
 // import NameChanger from "./components/LessonFive/NameChanger.tsx";
 // import NameChangerWithTitle from "./components/LessonFive/NameChangerWithTitle.tsx";
@@ -65,7 +66,8 @@ function App() {
         {/*Lesson 6*/}
         <Layout addClasses="bg-gray-50">
             {/*<AutoRedirect/>*/}
-            <AutoRedirectAdvanced/>
+            {/*<AutoRedirectAdvanced/>*/}
+            <WindowSize/>
         </Layout>
     </>
   )
