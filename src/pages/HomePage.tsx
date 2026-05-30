c8e3bbabea145ba5f0f16bdf573a4af2e5a47c05
GeorgiaR <grodopoulou95@gmail.com>
2026-05-30 18:45:54 +0300

    chore: add button components

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,11 @@
+import { Button } from "../components/ui/button";
+
 const HomePage = () => {
     return (
         <>
             <h1 className="text-center">Home Page</h1>
+            <Button>Button</Button>
         </>
     )
 }
-export default HomePage
+export default HomePage;
